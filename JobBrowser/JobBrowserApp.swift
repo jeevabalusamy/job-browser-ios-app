f0667e3e8446a554b137e22967b4def49d07408f
bjeeva <jeevanbalusamy@gmail.com>
2026-06-06 11:15:12 +0530

    Initial Commit

---
+++ JobBrowser/JobBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobBrowserApp.swift
+//  JobBrowser
+//
+//  Created by Jeeva Balusamy on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct JobBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JobBrowserTests/JobBrowserTests.swift
@@ -0,0 +1,19 @@
+//
+//  JobBrowserTests.swift
+//  JobBrowserTests
+//
+//  Created by Jeeva Balusamy on 06/06/26.
+//
+
+import Testing
+@testable import JobBrowser
+
+struct JobBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
